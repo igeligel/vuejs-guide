047a8203ab58dc081b8f7c864f486753d83c2ff9
igeligel <kevinigeligeligel@gmail.com>
2016-04-29 23:48:40 +0200

    Finished Class and Style Bindings
    Added Binding Inline Styles

---
+++ class-and-style-bindings/binding-inline-styles/array-syntax.js
@@ -0,0 +1,11 @@
+var vm = new Vue({
+    el: '#example',
+    data: {
+        styleObjectA: {
+            color: 'red'
+        },
+        styleObjectB: {
+            transform: 'rotate(-5deg)'
+        }
+    }
+})
---
+++ class-and-style-bindings/binding-inline-styles/object-syntax.js
@@ -0,0 +1,11 @@
+var vm = new Vue({
+    el: '#example',
+    data: {
+        activeColor: 'red',
+        fontSize: 30,
+        styleObject: {
+            color: 'red',
+            fontSize: '13px'
+        }
+    }
+})
