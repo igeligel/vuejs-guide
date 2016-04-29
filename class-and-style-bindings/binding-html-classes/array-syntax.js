d2f9c98ccd929e21ab0ba8e9bdd6a2e801dd4ed9
igeligel <kevinigeligeligel@gmail.com>
2016-04-29 22:55:06 +0200

    Added binding of html-classes

---
+++ class-and-style-bindings/binding-html-classes/array-syntax.js
@@ -0,0 +1,9 @@
+var vm = new Vue({
+    el: '#example',
+    data: {
+        classA: 'class-a',
+        classB: 'class-b',
+        isB: false,
+        isC: true
+    }
+})
---
+++ class-and-style-bindings/binding-html-classes/object-syntax.js
@@ -0,0 +1,11 @@
+var vm = new Vue({
+    el: '#example',
+    data: {
+        isA: true,
+        isB: false,
+        classObject: {
+            'class-a': true,
+            'class-b': false
+        }
+    }
+})
